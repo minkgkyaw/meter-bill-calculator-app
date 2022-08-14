58c05a61b519a21d13ea076821578210e2d22e98
minkgkyaw1999 <minkgkyaw1999@gmail.com>
2022-08-14 16:14:21 +0630

    chor: finished setup
    finished settin up project

--- android/app/src/main/java/com/meter_bill/MainActivity.java
+++ android/app/src/main/java/com/meter_bill/MainActivity.java
@@ -7,10 +7,11 @@ import com.facebook.react.ReactRootView;
 import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     SplashScreen.show(this); // here
-    super.onCreate(savedInstanceState);
+    super.onCreate(null);
   }
 
   /**
