47265ba0f13150e0093a3f65d80b168eb796f99e
minkgkyaw1999 <minkgkyaw1999@gmail.com>
2022-08-14 14:58:41 +0630

    setting up
    settingup project

--- android/app/src/main/java/com/meter_bill/MainActivity.java
+++ android/app/src/main/java/com/meter_bill/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "meter_bill";
+    return "MeterBill";
   }
 
   /**
--- ios/MeterBillTests/MeterBillTests.m
+++ ios/MeterBillTests/MeterBillTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface meter_billTests : XCTestCase
+@interface MeterBillTests : XCTestCase
 
 @end
 
-@implementation meter_billTests
+@implementation MeterBillTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
